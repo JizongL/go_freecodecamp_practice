14d81ea526e6414b6ca9b64cc24de4bb4ee2141b
Jizong Liang <Liang_Jizong@comcast.com>
2020-02-24 23:18:47 -0500

    second commit

--- go_operator.go
+++ go_operator.go
@@ -15,4 +15,32 @@ func main(){
 	fmt.Println(a | b) // 1011
 	fmt.Println(a ^ b) // 1001
 	fmt.Println(a &^ b) // 0100
+
+	 c:=8 // 2^3
+	 fmt.Println(c<<3)
+	 fmt.Println(c>>3)
+
+	//  floating point
+	m:= 3.14
+	// n = 13.7e72
+	// n=2.1E14
+	fmt.Printf("%v, %T\n",m,m)
+
+	// complex number
+	var t complex128 = 1+2i 
+	fmt.Printf("%v, %T\n", real(t),real(t))
+	fmt.Printf("%v, %T\n", imag(t),imag(t))
+
+	var p complex128 = complex(5,12)
+	fmt.Printf("%v, %T\n",p,p)
+
+	// string 
+	s:= "this is a string"
+	fmt.Printf("%v %T\n",s[2],s[2])
+
+	// string concatenation 
+	s2:="this is also a string"
+	fmt.Printf("%v, %T\n",s+s2,s+s2)
+	b := []byte(s2)
+	fmt.Printf("%v,%T\n",b,b)
 }
