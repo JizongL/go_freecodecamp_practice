2c8d738e5bb64f4d68902c6e6181a3fd788d4351
Jizong Liang <Liang_Jizong@comcast.com>
2020-03-01 18:56:12 -0500

    switch

---
+++ switch_go.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 10
+	switch {
+	case i <= 10:
+		fmt.Println("less than or equal to ten")
+		// fallthrough allow it continue to the next condition.
+		fallthrough
+	case i <= 20:
+		fmt.Println("less than or equal to twenty")
+	default:
+		fmt.Println("greater than twenty")
+	}
+}
