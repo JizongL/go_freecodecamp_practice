0dc2b5907b4ab7361815d1685f9f93d78ced55fc
Jizong Liang <Liang_Jizong@comcast.com>
2020-03-04 08:14:16 -0500

    interface 2

--- go_interface.go
+++ go_interface.go
@@ -1,3 +1,6 @@
+// interface naming convention, put er after the method.
+// e.g Write => Writer
+
 package main
 
 import (
@@ -5,10 +8,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello Go!"))
 }
 
 // only contain behaviors not content.
 type Writer interface {
 	Write([]byte) (int, error)
+}
+
+// implicitly impliment interface
+type ConsoleWriter struct{}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
 }
