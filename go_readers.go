45fc91a2513e05f86db66b803b2f3ba90bcb0598
Jizong Liang <Liang_Jizong@comcast.com>
2020-03-07 18:10:44 -0500

    io reader

--- go_readers.go
+++ go_readers.go
@@ -1,19 +1,38 @@
-package main
-
+package main 
 import (
 	"fmt"
 	"io"
-	"strings"
-)
+	
+	"os"
+	"io/ioutil"
+	"strings")
 
-func main() {
-	r := strings.NewReader("Hello, Reader!")
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n=%v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
+
+
+
+
+func main(){
+	// make a reader
+	//var r1 io.Reader
+	
+	r1,err := os.Open("./readme.md")
+	if err!=nil{
+		fmt.Println(err)
+	}
+	
+	buf, err := ioutil.ReadAll(r1)
+	fmt.Println(string(buf),"buf")
+	
+
+
+	// method of making a reader with strings
+	r:= strings.NewReader("Testing String")
+	arr:=make([]byte,8)
+	for{
+		n,err:= r.Read(arr)
+		fmt.Printf("n=%v err=%v b=%v\n",n,err,arr)
+		fmt.Printf("%q\n",arr[:n])
+		if err == io.EOF{
 			break
 		}
 	}
